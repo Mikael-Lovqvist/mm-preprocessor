7d92bbd2aeba44b428e76845763671be33ae7363
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2025-03-01 22:23:22 +0100

    Removed rudimentary tests/demos

--- test.js
+++
@@ -1,9 +0,0 @@
-function stuff() {
-	// %% if (condition) {
-	console.log("Oh yeah!");
-	/* %% } else if (other_thing) { */
-	console.log("Nicee");
-	/* %% } else { */
-	console.log("Nope!");
-	// %% }
-}
--- test4.js
+++
@@ -1,3 +0,0 @@
-console.log('Compile time stuff', /*@/ template.filename */);
-console.log('Debug stuff', /*@/D template_arguments */);
-console.log('Regular stuff', /*%# DEFAULT_MESSAGE */);
